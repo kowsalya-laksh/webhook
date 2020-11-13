2f6a6c014bad41ba41e5360152d82514aace9425
kowsalya-laksh <66460548+kowsalya-laksh@users.noreply.github.com>
2020-11-13 19:33:33 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,7 @@
 public class Helloworld {
    public static void main(String []args) {
       System.out.println("Hello World");
-       System.out.println("welcome");
+       System.out.println("welcome to webhook");
     
    }
 }
